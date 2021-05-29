80e57015ce286256f46e35c837fe3b941d18d9b6
Ivan <nighteliteace@gmail.com>
2021-05-29 15:26:37 +0200

    Added initial NN architecture with data loading, structuring and standardisation.

---
+++ nn.py
@@ -0,0 +1,422 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+###########################################
+
+# CONSTANTS DEFINING THE MODEL
+n_x = 8
+layers_dims = [8, 25, 17, 11, 1]  # 4-layer model
+
+###########################################
+
+
+def predict(X, y, parameters):
+    """
+    Predict the results of a  L-layer neural network.
+
+    Arguments:
+    X -- data set of examples you would like to label
+    parameters -- parameters of the trained model
+
+    Returns:
+    p -- predictions for the given dataset X
+    """
+
+    m = X.shape[1]
+    n = len(parameters) // 2  # number of layers in the neural network
+    p = np.zeros((1, m))
+
+    # Forward propagation
+    probas, caches = L_model_forward(X, parameters)
+
+    # convert probas to 0/1 predictions
+    for i in range(0, probas.shape[1]):
+        if probas[0, i] >= 0.5:
+            p[0, i] = 1
+        else:
+            p[0, i] = 0
+
+    print("Accuracy: " + str(np.sum((p == y) / m) * 100), "%")
+
+    return p
+
+
+def sigmoid(X):
+    return 1 / (1 + np.exp(-X)), X
+
+
+def relu(X):
+    return np.maximum(0.01 * X, X), X
+
+
+def tanh(X):
+    return 2 * sigmoid(X * 2)[0] - 1, X
+
+
+def tanh_backward(dA, Z):
+    return (1 - tanh(Z)[0] ** 2) * dA
+
+
+def relu_backward(dA, Z):
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0.01
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
+
+
+def sigmoid_backward(dA, Z):
+    s = 1 / (1 + np.exp(-Z))
+    dZ = dA * s * (1 - s)
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
+
+
+def softmax(X):
+    expo = np.exp(X)
+    expo_sum = np.sum(np.exp(X))
+    return expo / expo_sum
+
+
+def initialize_parameters_deep(layer_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    bl -- bias vector of shape (layer_dims[l], 1)
+    """
+
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+
+def linear_forward(A, W, b):
+    """
+    Linear part of a layer's forward propagation.
+
+    Arguments:
+    A -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+
+    Returns:
+    Z -- the input of the activation function, also called pre-activation parameter
+    cache -- a python tuple containing "A", "W" and "b" ; stored for computing the backward pass efficiently
+    """
+
+    Z = np.dot(W, A) + b
+
+    assert (Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+
+    return Z, cache
+
+
+def linear_activation_forward(A_prev, W, b, activation):
+    """
+    Forward propagation for the LINEAR->ACTIVATION layer
+
+    Arguments:
+    A_prev -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    A -- the output of the activation function, also called the post-activation value
+    cache -- a python tuple containing "linear_cache" and "activation_cache";
+             stored for computing the backward pass efficiently
+    """
+
+    if activation == "sigmoid":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = sigmoid(Z)
+
+    elif activation == "relu":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = relu(Z)
+    elif activation == "tanh":
+        Z, linear_cache = linear_forward(A_prev, W, b)
+        A, activation_cache = tanh(Z)
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+
+def L_model_forward(X, parameters):
+    """
+    Forward propagation for the [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID computation
+
+    Arguments:
+    X -- data, numpy array of shape (input size, number of examples)
+    parameters -- output of initialize_parameters_deep()
+
+    Returns:
+    AL -- last post-activation value
+    caches -- list of caches containing:
+                every cache of linear_activation_forward() (there are L-1 of them, indexed from 0 to L-1)
+    """
+
+    caches = []
+    A = X
+    L = len(parameters) // 2  # number of layers in the neural network
+
+    # [LINEAR -> TanH]*(L-1)
+    for l in range(1, L):
+        A_prev = A
+        A, cache = linear_activation_forward(A_prev, parameters["W" + str(l)], parameters["b" + str(l)], 'tanh')
+        caches.append(cache)
+
+    # LINEAR -> SIGMOID
+    AL, cache = linear_activation_forward(A, parameters["W" + str(L)], parameters["b" + str(L)], 'sigmoid')
+    caches.append(cache)
+
+    assert (AL.shape == (1, X.shape[1]))
+
+    return AL, caches
+
+
+def compute_cost(AL, Y):
+    """
+    Cost function defined by equation (7).
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (1, number of examples)
+    Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
+
+    Returns:
+    cost -- cross-entropy cost
+    """
+
+    m = Y.shape[0]
+
+    # Computes loss from aL and y.
+    cost = -(1 / m) * np.sum(Y * np.log(AL) + (1 - Y) * np.log(1 - AL))  # moze i np.multiply(...)
+    cost = np.squeeze(cost)  # Boze pomozi... zasto ovo postoji :(
+
+    assert (cost.shape == ())
+
+    return cost
+
+
+def linear_backward(dZ, cache):
+    """
+    Linear portion of backward propagation for a single layer (layer l)
+
+    Arguments:
+    dZ -- Gradient of the cost with respect to the linear output (of current layer l)
+    cache -- tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = (1 / m) * np.dot(dZ, A_prev.T)
+    db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ)
+
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+
+    return dA_prev, dW, db
+
+
+def linear_activation_backward(dA, cache, activation):
+    """
+    Backward propagation for the LINEAR->ACTIVATION layer.
+
+    Arguments:
+    dA -- post-activation gradient for current layer l
+    cache -- tuple of values (linear_cache, activation_cache) we store for computing backward propagation efficiently
+    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    elif activation == "sigmoid":
+        dZ = sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+    elif activation == "tanh":
+        dZ = tanh_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+
+def L_model_backward(AL, Y, caches):
+    """
+    Backward propagation for the [LINEAR->RELU] * (L-1) -> LINEAR -> SIGMOID group
+
+    Arguments:
+    AL -- probability vector, output of the forward propagation (L_model_forward())
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat)
+    caches -- list of caches containing:
+                every cache of linear_activation_forward() with "relu" (it's caches[l], for l in range(L-1) i.e l = 0...L-2)
+                the cache of linear_activation_forward() with "sigmoid" (it's caches[L-1])
+
+    Returns:
+    grads -- A dictionary with the gradients
+             grads["dA" + str(l)] = ...
+             grads["dW" + str(l)] = ...
+             grads["db" + str(l)] = ...
+    """
+    grads = {}
+    L = len(caches)  # the number of layers
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)  # after this line, Y is the same shape as AL
+
+    # Initializing the backpropagation
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+    # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "dAL, current_cache". Outputs: "grads["dAL-1"], grads["dWL"], grads["dbL"]
+    current_cache = caches[L - 1]
+    grads["dA" + str(L - 1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL,
+                                                                                                      current_cache,
+                                                                                                      activation='sigmoid')
+
+    # Loop from l=L-2 to l=0
+    for l in reversed(range(L - 1)):
+        # lth layer: (RELU -> LINEAR) gradients.
+        # Inputs: "grads["dA" + str(l + 1)], current_cache". Outputs: "grads["dA" + str(l)] , grads["dW" + str(l + 1)] , grads["db" + str(l + 1)]
+
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 1)], current_cache,
+                                                                    activation='tanh')
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate, change):
+    """
+    Update parameters using gradient descent
+
+    Arguments:
+    parameters -- python dictionary containing your parameters
+    grads -- python dictionary containing your gradients, output of L_model_backward
+
+    Returns:
+    parameters -- python dictionary containing your updated parameters
+                  parameters["W" + str(l)] = ...
+                  parameters["b" + str(l)] = ...
+    """
+    L = len(parameters) // 2  # number of layers in the neural network
+
+    for l in range(L):
+        new_change_W = learning_rate * grads["dW" + str(l + 1)] + 0.9 * change["W" + str(l + 1)]
+        new_change_b = learning_rate * grads["db" + str(l + 1)] + 0.9 * change["b" + str(l + 1)]
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - new_change_W
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - new_change_b
+        change["W" + str(l + 1)] = new_change_W
+        change["b" + str(l + 1)] = new_change_b
+        # parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+        # parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+    return parameters
+
+
+def L_layer_model(X, Y, layers_dims, learning_rate=0.01, num_iterations=10000, print_cost=False,
+                  dynamic_grad_change=False, point_of_change=None, second_value=None):
+    """
+    Implements a L-layer neural network: [LINEAR->TANH]*(L-1)->LINEAR->SIGMOID.
+
+    Arguments:
+    X -- data, numpy array of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+    dynamic_grad_change -- if True, gradient is dynamically shrunken as time progresses
+    point_of_change -- defines tipping point where second_value lr becomes active
+    second_value -- second lr val that becomes active when point_of_change is passed
+
+    Returns:
+    parameters -- computed weights TODO: (sacuvaj ovo na disk Pope, da ima neka opcija za to)
+    """
+
+    np.random.seed(1)
+    costs = []  # keep track of cost
+
+    # Parameters initialization. (≈ 1 line of code)
+    parameters = initialize_parameters_deep(layers_dims)
+
+    # Loop (gradient descent)
+    change = {}
+    for i in range(len(layers_dims)):
+        change["W" + str(i + 1)] = 0.0
+        change["b" + str(i + 1)] = 0.0
+
+    for i in range(0, num_iterations):
+
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        AL, caches = L_model_forward(X, parameters)
+
+        # Compute cost.
+        cost = compute_cost(AL, Y)
+
+        if dynamic_grad_change:
+            if i % 3000 == 0 and i > 0:
+                learning_rate /= (i / 1000)
+        elif point_of_change is not None and second_value is not None:
+            if cost < point_of_change:
+                learning_rate = second_value
+
+        # Backward propagation.
+        grads = L_model_backward(AL, Y, caches)
+
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate, change)
+
+        # Print the cost every 100 training example
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+
+    # plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
---
+++ utils.py
@@ -0,0 +1,138 @@
+from nn import *
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+def load_data():
+    train_set_x_orig = []
+    train_set_y_orig = []
+
+    test_set_x_orig = []
+    test_set_y_orig = []
+
+    categories = {'1': [1, 0, 0], '2': [0, 1, 0], '3': [0, 0, 1]}
+
+    with open('datasets/train.csv') as f:
+        lines = f.readlines()
+        train_data = lines[1:]
+
+    with open('datasets/test.csv') as f:
+        lines = f.readlines()
+        test_data = lines[1:]
+
+    with open('datasets/gender_submission.csv') as f:
+        lines = f.readlines()
+        test_results = lines[1:]
+
+    for line in train_data:
+        if "\",," in line:
+            continue
+        tokens = line.split(",")
+        test_case = []
+        try:
+            test_case.append(float(tokens[-7]))
+        except:
+            continue
+        test_case.append(float(tokens[-6]))
+        test_case.append(float(tokens[-5]))
+        test_case.append(float(tokens[-3]))
+        if tokens[-8] == "male":
+            test_case.append(1.0)
+        else:
+            test_case.append(0.0)
+        for val in categories[tokens[2].replace("\n", "")]:
+            test_case.append(val)
+        train_set_x_orig.append(test_case)
+        train_set_y_orig.append(float(tokens[1]))
+
+    for i, line in enumerate(test_data):
+        if "\",," in line or "e,," in line:
+            continue
+        tokens = line.split(",")
+        test_case = []
+        test_case.append(float(tokens[-7]))
+        test_case.append(float(tokens[-6]))
+        test_case.append(float(tokens[-5]))
+        try:
+            test_case.append(float(tokens[-3]))
+        except:
+            continue
+        if tokens[-8] == "male":
+            test_case.append(1.0)
+        else:
+            test_case.append(0.0)
+        for val in categories[tokens[1].replace("\n", "")]:
+            test_case.append(val)
+        test_set_x_orig.append(test_case)
+        test_set_y_orig.append(float(test_results[i].split(",")[1]))
+        i += 1
+
+    return np.array(train_set_x_orig), np.array(train_set_y_orig), np.array(test_set_x_orig), np.array(
+        test_set_y_orig), [b'died', b'survived']
+
+
+def get_structured_data():
+    train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
+
+    dev_set_size = (len(train_x_orig) // 5)
+
+    rng = np.random.default_rng(5035)
+    indexes = rng.choice(len(train_x_orig), size=dev_set_size, replace=False)
+
+    dev_x = []
+    dev_y = []
+
+    for i in indexes:
+        dev_x.append(train_x_orig[i])
+        dev_y.append(train_y[i])
+
+    np.delete(train_y, indexes)
+    np.delete(train_x_orig, indexes)
+
+    dev_x = np.array(dev_x)
+    dev_y = np.array(dev_y)
+
+    return train_x_orig, train_y, test_x_orig, test_y, dev_x, dev_y
+
+
+def standardise_data(train_x_orig, test_x_orig, dev_x, print_cor=False, print_boxplot=False, train_y=None):
+    train_arr = []
+    dev_arr = []
+    test_arr = []
+    for i in range(n_x):
+        for el in train_x_orig:
+            train_arr.append(el[i])
+        for el in dev_x:
+            dev_arr.append(el[i])
+        for el in test_x_orig:
+            test_arr.append(el[i])
+
+        data_train = np.array(train_arr)
+        data_dev = np.array(dev_arr)
+        data_test = np.array(test_arr)
+
+        mean_train = np.mean(data_train)
+        sd_train = np.sqrt(np.var(data_train))
+        mean_dev = np.mean(data_dev)
+        sd_dev = np.sqrt(np.var(data_dev))
+        mean_test = np.mean(data_test)
+        sd_test = np.sqrt(np.var(data_test))
+        for el in train_x_orig:
+            el[i] = (el[i] - mean_train) / sd_train
+        for el in dev_x:
+            el[i] = (el[i] - mean_dev) / sd_dev
+        for el in test_x_orig:
+            el[i] = (el[i] - mean_test) / sd_test
+
+        if print_cor:
+            print(np.corrcoef(data_train, train_y))
+            print()
+        if print_boxplot:
+            plt.boxplot(data_train, notch=None, vert=None, patch_artist=None, widths=None)
+            plt.show()
+
+        train_arr.clear()
+        dev_arr.clear()
+        test_arr.clear()
