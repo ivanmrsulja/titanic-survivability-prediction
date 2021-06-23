8ff7ebb99d43bf219e07c307ae42f8468e87f2d8
Ivan Mrsulja <ivan.mrsulja@uns.ac.rs>
2021-06-23 15:20:25 +0200

    Cleanup

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ if __name__ == '__main__':
     train_and_evaluate_nn(train_x, train_y, test_x, test_y, dev_x, dev_y)
 
     # Decision tree
-    model = train_decision_tree(train_x, train_y)
-    evaluate_linear_model(model, test_x, test_y, "DECISION TREE", plot=False)  # works better with non-standardised data
+    # model = train_decision_tree(train_x, train_y)
+    # evaluate_linear_model(model, test_x, test_y, "DECISION TREE", plot=False)  # works better with non-standardised data
 
     # Lasso regression
-    model = train_lasso(train_x, train_y)
-    evaluate_linear_model(model, test_x, test_y, "LASSO", plot=False)
+    # model = train_lasso(train_x, train_y)
+    # evaluate_linear_model(model, test_x, test_y, "LASSO", plot=False)
 
     # Support Vector Machine
-    model = train_svm(train_x, train_y, dfs="ovo")
-    evaluate_linear_model(model, test_x, test_y, "SUPPORT VECTOR MACHINE", plot=True)
+    # model = train_svm(train_x, train_y, dfs="ovo")
+    # evaluate_linear_model(model, test_x, test_y, "SUPPORT VECTOR MACHINE", plot=True)
 
     # Ensemble (DT + SVM)
-    model = train_ensemble(train_x, train_y)
-    evaluate_linear_model(model, test_x, test_y, "ENSEMBLE", plot=False)
+    # model = train_ensemble(train_x, train_y)
+    # evaluate_linear_model(model, test_x, test_y, "ENSEMBLE", plot=False)
 
     # Naive Bayes
-    train_and_evaluate_naive_bayes(train_x_orig, train_y_orig, test_x_orig, test_y_orig, dev_x_orig, dev_y_orig)
+    # train_and_evaluate_naive_bayes(train_x_orig, train_y_orig, test_x_orig, test_y_orig, dev_x_orig, dev_y_orig)
