0d5d081536bde3ab1bb7a1c8b24a15726542a5b1
Ivan <nighteliteace@gmail.com>
2021-06-25 12:38:29 +0200

    Added option to save weights for quicker demonstration.

--- training.py
+++ training.py
@@ -6,22 +6,30 @@ from sklearn import svm
 from sklearn.ensemble import VotingClassifier
 from nn import *
 from naive_bayes import NaiveBayes
+import pickle
 
 plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 
-def train_and_evaluate_nn(train_x_orig, train_y, test_x_orig, test_y, dev_x, dev_y):
+def train_and_evaluate_nn(train_x_orig, train_y, test_x_orig, test_y, dev_x, dev_y, save_weights=False,
+                          use_loaded_weights=False):
     train_x = train_x_orig.reshape(train_x_orig.shape[0], -1).T
     dev_x = dev_x.reshape(dev_x.shape[0], -1).T
     test_x = test_x_orig.reshape(test_x_orig.shape[0], -1).T
 
-    parameters = L_layer_model(train_x, train_y, layers_dims, dev_x, dev_y, learning_rate=.25, num_iterations=20000,
-                               print_cost=True, dynamic_grad_change=False, point_of_change=0.31, second_value=0.03)
+    if use_loaded_weights:
+        print("Loading pre-trained layers...")
+        with open("weights", "rb") as infile:
+            parameters = pickle.load(infile)
+        print("Done.")
+    else:
+        parameters = L_layer_model(train_x, train_y, layers_dims, dev_x, dev_y, learning_rate=.25, num_iterations=20000,
+                                   print_cost=True, dynamic_grad_change=False, point_of_change=0.31, second_value=0.03)
 
     # parameters = L_layer_model(train_x, train_y, layers_dims, dev_x, dev_y, learning_rate=.25, num_iterations=20000,
-    #                            print_cost=True, dynamic_grad_change=False, point_of_change=0.31, second_value=0.02)
+    #                           print_cost=True, dynamic_grad_change=False, point_of_change=0.31, second_value=0.02)
 
     print("\n*NEURAL NET:")
     print("Training", end=" ")
@@ -30,6 +38,10 @@ def train_and_evaluate_nn(train_x_orig, train_y, test_x_orig, test_y, dev_x, dev
     pred_dev = predict(dev_x, dev_y, parameters)
     print("Test", end=" ")
     pred_test = predict(test_x, test_y, parameters)
+
+    if save_weights:
+        with open("weights", "wb") as outfile:
+            pickle.dump(parameters, outfile)
 
 
 def train_decision_tree(train_x_orig, train_y):
